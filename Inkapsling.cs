fafd8d8f4ae98c46dbc423703921b148ad8fa02a
b1-loop <bojidarivanov98@gmail.com>
2025-09-13 14:46:52 +0200

    skapat Inkapsling branch

---
+++ Inkapsling.cs
@@ -0,0 +1,26 @@
+﻿
+
+namespace OOP_GITHUB
+{
+    public class Inkapsling
+    {
+        private int money;  //denna ska vara privat ingen ska se personens pengar
+
+        public Inkapsling(int Money) //konstruktor som tar Money som parameter
+        {
+            money = Money; //sätter money till Money det vill säga jag kan sätta värde på money när jag skapar objekt av klassen
+        }
+
+        public int Money
+        {
+            get { return money; }  // get metoden returnerar money
+            set { money = value; } // set metoden sätter money till value det vill säga jag kan låsa upp den privata variabeln i program cs så länge
+            //jag använder set metoden så länge jag skriver ett int värde inom parentesen så kan jag retura värdet och göra den public då
+        }
+
+        public void ShowMoney() //min metod ShowMoney som visar hur mycket pengar någon har
+        {
+            Console.WriteLine($"Hon tjänar {money} kr förre skatt.");
+        }
+    }   
+}
