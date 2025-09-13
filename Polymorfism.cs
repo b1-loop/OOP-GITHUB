ed53d9d727e270ca8b1971a8f313c16a12f7c525
b1-loop <bojidarivanov98@gmail.com>
2025-09-13 15:05:14 +0200

    Polymorfism branch för en till person

---
+++ Polymorfism.cs
@@ -0,0 +1,12 @@
+﻿
+namespace OOP_GITHUB
+{
+    public class Polymorfism : Person  //skapat en ny klass som heter polymorfism som ärver från person klassen
+    {
+        public override void IntroduceYourself() //override nyckelordet används för att ändra en metod som ärvs från en basklass
+        {
+            Console.WriteLine($"Hi, I'm {Name} {Surname}, a {Age}-year-old individual."); //override metoden IntroduceYourself från person klassen och ändrar presentationen
+        }
+
+    }
+}
