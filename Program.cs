be41afb97be48634472ea202523dbc15a40c5899
b1-loop <bojidarivanov98@gmail.com>
2025-09-13 13:42:43 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OOP_GITHUB
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
